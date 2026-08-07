b2db1e1a2750ba9f88fb0faa6b20d0c2b78a7836
Harshit Choudhary <harshit1840077@gmail.com>
2026-08-07 10:27:10 +0530

    config HRMS Project system with db context setup

---
+++ Data/HRMSDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using HRMS.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HRMS.Api.Data
+{
+public class HRMSDbContext: DbContext
+{
+        public HRMSDbContext(DbContextOptions<HRMSDbContext> options) 
+            : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace HRMS.Api.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Role { get; set; }
+    }
+}
